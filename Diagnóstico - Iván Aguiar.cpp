4a6b3bdc75c64ab73a138e9fb15db25fef8b81b6
MeiAgiaru <57736305+MeiAgiaru@users.noreply.github.com>
2020-04-03 17:49:42 -0300

    Add files via upload
    Ahora sí algo de EDI (?)
    Ejercicio diagnóstico.

---
+++ Diagnóstico - Iván Aguiar.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "Enter your name here: \n\n";
+
+    cin >> name; 
+
+    cout << "\n Hello " << name <<". Have a nice day and remember... \n\n";
+
+    cout << "#StayInHome \n";
+    
+    return 0;
+}
+
